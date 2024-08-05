5ee2e22b958197efcd8b4203789dfa527b36f663
Yadvendra124 <23bit176iet.davv.edu>
2024-08-05 15:46:22 +0530

    Print Even Number

--- Pattern2.java
+++ Pattern2.java
@@ -7,7 +7,7 @@ public class Pattern2 {
             }
             System.out.println();
         }
-        for(int i=0;i<=n;i++){
+        for(int i=n;i>=0;i--){
             for(int j=0;j<=n-i;j++){
                 System.out.print(" ");
             }
@@ -16,7 +16,16 @@ public class Pattern2 {
             }
             System.out.println();
         }
-
+        for(int i=0;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
         for (int i = 0; i < n; i++) {
             char ch = 'A';
             for (int j = 0; j <= i; j++) {
@@ -25,8 +34,20 @@ public class Pattern2 {
             }
             System.out.println();
         }
-
-
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n-i+1);j++){
+            System.out.print("*");
+        }
+        int space =2*(i-1);
+        for(int j =1;j<=space;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=(n-i+1);j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        
+       }
     }
 }
 
