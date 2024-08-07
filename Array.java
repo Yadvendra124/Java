3fdedd41859dc14664e3cabffe728c9a1fa60467
Yadvendra124 <23bit176iet.davv.edu>
2024-08-07 12:16:32 +0530

    Array

--- Array.java
+++ Array.java
@@ -13,7 +13,7 @@ public class Array {
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
-            System.out.print(arr[i][j]+"  ");
+            System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
