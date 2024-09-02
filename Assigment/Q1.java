2159324a4a43ec2641f4c2ad0fe9db8dcb3f607e
Yadvendra124 <23bit176iet.davv.edu>
2024-09-02 22:14:38 +0530

    Q-1

---
+++ Assigment/Q1.java
@@ -0,0 +1,31 @@
+
+package Assigment;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Choose the temprature");
+        System.out.println("Press 1 for celsius");
+        System.out.println("Press 2 for Fahrenheit");
+        int a = sc.nextInt();
+        if(a==1){
+            System.out.println("Enter the Temprature :");
+            double x = sc.nextDouble();
+            double y = x*(1.8) + 32 ;
+            System.out.println(x + "°C = "+y+"°F");
+        }
+        else if(a==2){
+            System.out.println("Enter the Temprature :");
+            double x = sc.nextDouble();
+            double y = (x-32)*0.55 ;
+            System.out.println(x + "°F = "+y+"°C");
+
+        }
+        else{
+            System.out.println("Invalid ");
+        }
+    }
+    
+}
