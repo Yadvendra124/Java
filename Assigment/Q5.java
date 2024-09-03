5dad16bb4508a023a87a2bbbd18fbe4a943b9ea4
Yadvendra124 <23bit176iet.davv.edu>
2024-09-03 14:32:27 +0530

    Q-5

---
+++ Assigment/Q5.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Miles : ");
+        double x = sc.nextDouble();
+        double y = ( x*1.609);
+        System.out.println(y+" kilometer");
+
+    }
+    
+}
