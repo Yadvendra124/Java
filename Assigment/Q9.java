ec623465fc7359a758c288c7b5d3a48fc940e035
Yadvendra124 <23bit176iet.davv.edu>
2024-09-06 17:34:36 +0530

    Q9

---
+++ Assigment/Q9.java
@@ -0,0 +1,13 @@
+
+
+public class Q9 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 1;
+        for(int i=1;i<=a;i++){
+            b = b*i;
+        }
+        System.out.println(b);
+    }
+    
+}
