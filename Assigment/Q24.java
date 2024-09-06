97f4cc970d5c2123d30daeb2820d56b344640905
Yadvendra124 <23bit176iet.davv.edu>
2024-09-06 23:19:38 +0530

    Q-24

--- Assigment/Q24.java
+++ Assigment/Q24.java
@@ -1,3 +1,23 @@
+
+import java.util.*;
+
 public class Q24 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number of element :");
+        int n = sc.nextInt();
+        int[] arr =  new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i=0;i<n;i++){ 
+            sum+=arr[i];
+        }
+        double average = sum/n;
+        System.out.println("Sum : "+sum);
+        System.out.println("Average : "+average);
+        
+    }
 }
