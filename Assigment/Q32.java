cdd5c8e615129a1ad65baae85337a5c356afefe4
Yadvendra124 <23bit176iet.davv.edu>
2024-09-07 09:49:56 +0530

    Q-32

--- Assigment/Q32.java
+++ Assigment/Q32.java
@@ -7,7 +7,7 @@ class Detial{
     }
     public void display(){
         System.out.println("Name : "+name);
-        System.out.println("Age : "+age);;
+        System.out.println("Age : "+age);
     }
 
     
