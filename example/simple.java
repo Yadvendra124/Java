ab4742876fc6f067485ec373bc512e94873646e9
Yadvendra124 <23bit176iet.davv.edu>
2024-09-10 16:29:56 +0530

    package

---
+++ example/simple.java
@@ -0,0 +1,11 @@
+package example;
+
+public class simple {
+    public void show(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        simple s1 = new simple();
+        s1.show();;
+    }
+}
---
+++ packagess.java
@@ -0,0 +1,8 @@
+import example.simple;
+public class packagess {
+    public static void main(String[] args) {
+        simple s2 = new simple();
+        s2.show();
+        
+    }
+}
